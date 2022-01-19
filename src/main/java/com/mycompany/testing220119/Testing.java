04c319df36c8a95259f7ffd387c514a6979a8e76
ZhiKai0119 <chongzk-wm19@student.tarc.edu.my>
2022-01-19 14:28:58 +0800

    Kai0119

--- src/main/java/com/mycompany/testing220119/Testing.java
+++ src/main/java/com/mycompany/testing220119/Testing.java
@@ -12,5 +12,6 @@ package com.mycompany.testing220119;
 public class Testing {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Testing");
     }
 }
