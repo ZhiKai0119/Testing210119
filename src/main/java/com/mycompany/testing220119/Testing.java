aebdbef0e1f6eb7f202270663b77bc68f8b1fa46
Zhi Kai <Zhi Kai@LAPTOP-9JJSG0F8>
2022-01-19 13:47:04 +0800

    Kai0119

---
+++ src/main/java/com/mycompany/testing220119/Testing.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.testing220119;
+
+/**
+ *
+ * @author Zhi Kai
+ */
+public class Testing {
+    
+}
